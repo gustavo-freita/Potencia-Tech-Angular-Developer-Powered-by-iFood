caaaebf02f908444453dccc94b7ce132befae1df
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-05-19 20:38:11 -0300

    adicionando mais conteudo das aulas de Angular

--- Angular/Diretivas/diretivas-proj/src/app/app.module.ts
+++ Angular/Diretivas/diretivas-proj/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
+import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    CardComponent,
+    CompAtributosComponent
   ],
   imports: [
     BrowserModule,
